84b1cc95db245799d676436ddc6531ba82803585
mo3az.abdellatif <mo3az.abdellatif@gmail.com>
2020-01-22 13:07:56 +0200

    Updates

---
+++ precache-manifest.3aae99632615c8960c8a323489e8d7ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccd5194053eda457fb0cf438a0c589e4",
+    "url": "/apptask/index.html"
+  },
+  {
+    "revision": "6266ed504e3e66e5a9ee",
+    "url": "/apptask/static/css/main.56165426.chunk.css"
+  },
+  {
+    "revision": "41081c59ee3bcacb08b6",
+    "url": "/apptask/static/js/2.9b27aa9f.chunk.js"
+  },
+  {
+    "revision": "5a0d989568a936270ef6c2e0e2d0229c",
+    "url": "/apptask/static/js/2.9b27aa9f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6266ed504e3e66e5a9ee",
+    "url": "/apptask/static/js/main.fd65c9d9.chunk.js"
+  },
+  {
+    "revision": "177116d3f187ff9481dd",
+    "url": "/apptask/static/js/runtime-main.dbcc1923.js"
+  }
+]);
